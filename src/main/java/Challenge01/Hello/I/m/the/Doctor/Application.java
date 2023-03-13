a8762e2076365ea3359564e7369c478597ed4495
Apollo02112022 <nicolas.fuente@sncf.fr>
2023-03-13 16:20:20 +0100

    second commit

---
+++ src/main/java/Challenge01/Hello/I/m/the/Doctor/Application.java
@@ -0,0 +1,13 @@
+package Challenge01.Hello.I.m.the.Doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
